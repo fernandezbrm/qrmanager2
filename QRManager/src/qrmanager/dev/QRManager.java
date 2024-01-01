d3fb40f9143f6b4e7511b035ea658eba439b7a9d
Roberto Fernandez <fernandezbrm@gmail.com>
2024-01-01 11:57:55 -0600

    QRManager: change LED 13 running state blinking to 3 seconds. Clean up indentantion

--- QRManager/src/qrmanager/dev/QRManager.java
+++ QRManager/src/qrmanager/dev/QRManager.java
@@ -22,24 +22,37 @@ import org.apache.logging.log4j.core.config.Configurator;
  *
  */
 public class QRManager implements SerialPortListener, Runnable{
-	// Class attributes
+	/** Class attributes */
 	private static ReadConfig myReadConfig = new ReadConfig();
 	// By default use constant path, it can be overriden by CLI argument
 	private static String configFilePath = Constants.CONF_FILE_PATH_DEFAULT;
+	// List of QRManager instances
 	private static List<QRManagerDTO> myQRMDTO = new ArrayList<QRManagerDTO>();
+	// Digital pins serial adapter singleton instance
 	private static DigitalSerialIOImpl dSerialImpl;
+	// QR JSON schema elements to validated
 	private static List <String> schemaElements;
+	// Recovery time out. NOT USED
 	private static final int RETRY_PERIOD_MS   = 5000;
+	// Activity LED attributes
 	private static final int RUN_LED_PIN 	   = 13;
-	private static final int RUN_LED_PERIOD_MS = 1000;
+	private static final int RUN_LED_PERIOD_MS = 3000;
 	private static final String QRMANAGER_NAME = "QRManager Main Thread";
+	// Logger
 	private static Logger logger = LogManager.getLogger(QRManager.class);
-	// Instance attributes
+
+	/** Instance attributes */
+	// QRManager configuration parameters DTO
 	private QRManagerDTO myQRManagerDTO;
+	// QR reader port
 	private RxTxSerialPort myQRReader;
+	// Digital pins serial adapter singleton instance
 	private DigitalIOInterface myDio;
+	// Digital pin to control barrier
 	private int doChannel;
+	// Barrier activation pulse length in milliseconds
 	private int pulseLengthMs;
+	// QRManager instance logging name
 	private String myName;
 	
 	/** 
@@ -60,34 +73,37 @@ public class QRManager implements SerialPortListener, Runnable{
 	 * @throws Exception */
 	QRManager(QRManagerDTO qrManagerDTO, DigitalIOInterface dSerialIO) throws Exception {
 		logger.debug("QRManager: name = " + qrManagerDTO.getName() + 
-							", portName = " + qrManagerDTO.getPortName() + 
-							", speed = " + qrManagerDTO.getSpeed() + 
-							", doChannel = " + qrManagerDTO.getDoChannel() + 
-							" pulseLengthMs = " + qrManagerDTO.getpulseLenghtMs());
+			     ", portName = " + qrManagerDTO.getPortName() + 
+			     ", speed = " + qrManagerDTO.getSpeed() + 
+			     ", doChannel = " + qrManagerDTO.getDoChannel() + 
+			     " pulseLengthMs = " + qrManagerDTO.getpulseLenghtMs());
 		
 		// Save our DTO
 		this.myQRManagerDTO = qrManagerDTO;
 		
-		/** Create RxTxReaderSerialPort */
+		// Create RxTxReaderSerialPort
 		setMyQRReader(new RxTxSerialPort(qrManagerDTO.getPortName(), qrManagerDTO.getSpeed(), this));
 		logger.debug("QR reader serial port interface created");
 		
-		// Save the digital output channel bound to this QRManager instance
+		// Save the barrier activation digital output channel bound to this QRManager instance
 		this.doChannel = qrManagerDTO.getDoChannel();
 		
 		// Name to identify this QRManager instance
 		this.myName = qrManagerDTO.getName();
 		
-		// Pulse length in milliseconds
+		// Barrier activation pulse length in milliseconds
 		this.pulseLengthMs = qrManagerDTO.getpulseLenghtMs();
 		
-		/** Set digital output interface singleton instance reference*/
+		// Set digital output interface singleton instance reference
 		setMyDio(dSerialIO);
 		logger.debug("Digital output interface singleton gotten");
 	}
 
+	/** Validate read QR JSON string versus expected elements. 
+	 *  Returns true if valid JSON, otherwise false            
+ 	 */
 	private boolean validateJsonSchema(String qr) {
-		// Validate that elements in schema present in read QR JSOn structure
+		// Validate that elements in schema are present in read QR JSON structure
 		for (String element: schemaElements) {
 			if (!qr.contains(element)) {
 				return false;
@@ -102,36 +118,35 @@ public class QRManager implements SerialPortListener, Runnable{
 	public void dataReceived(String data) {
 		// Check if out thread reported an error and terminated
 		if (data.contains("ERROR: reading port")) {
-			// We got a fatal error with the QR reader serial to USB converter, try to recover
-			logger.error(data);
-			// Destroy our RxTxSerialPort instance
-			setMyQRReader(null);
-			// Launch recovery thread
-			// Thread thread = new Thread(this);
-			// thread.start();
+		   // We got a fatal error with the QR reader serial to USB converter, try to recover
+		   logger.error(data);
+		   // Destroy our RxTxSerialPort instance
+		   setMyQRReader(null);
+		   // Launch recovery thread
+		   // Thread thread = new Thread(this);
+		   // thread.start();
 		}
 		// Validate if QR matches schema JSON structure
 		else if (validateJsonSchema(data)) {
-			// System.out.println(">>>> " + myName + " QR VALID = " + data + " ACCESS GRANTED");
-			logger.info(">>>> " + myName + " QR VALID = " + data + " ACCESS GRANTED");
-			// If QR valid, trigger momentary corresponding DO output
-			// in order to open lane barrier
-			this.getMyDio().setOutputOn(myName, doChannel); 
-			try {
-				Thread.sleep(pulseLengthMs); 
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-			this.getMyDio().setOutputOff(myName, doChannel);
+		   logger.info(">>>> " + myName + " QR VALID = " + data + " ACCESS GRANTED");
+		   // If QR valid, trigger momentary corresponding DO output
+		   // in order to open lane barrier
+		   this.getMyDio().setOutputOn(myName, doChannel); 
+		   try {
+		      Thread.sleep(pulseLengthMs); 
+		   } catch (InterruptedException e) {
+		      // TODO Auto-generated catch block
+		      e.printStackTrace();
+		   } 
+		   // Turn off barrier activation DO output
+		   this.getMyDio().setOutputOff(myName, doChannel);
 		}
 		else {
-			// System.out.println("<<<< " + myName + ": QR INVALID = " + data+ " ACCESS DENIED!!!!");
-			logger.info("<<<< " + myName + ": QR INVALID = " + data+ " ACCESS DENIED!!!!");
+		   logger.info("<<<< " + myName + ": QR INVALID = " + data+ " ACCESS DENIED!!!!");
 		}
 	}
 	
-	// Recovery thread method
+	/**  Serial port recovery thread method. NOT USED */
 	public void run() {
 		boolean exit = false;
 		
@@ -182,84 +197,87 @@ public class QRManager implements SerialPortListener, Runnable{
 	 */
 	public static void main(String[] args) throws Exception {
 	    // Read command line arguments
-        logger.info("Argument count: " + args.length);
+            logger.info("Argument count: " + args.length);
 	    for (int i = 0; i < args.length; i++) {
 	    	logger.info("Argument " + i + ": " + args[i]);
 	        if (0 == i) {
-	        	configFilePath = args[i];
+		  // First argument is path to qrmanager_conf.j
+	          configFilePath = args[i];
 	        }
 	    }
 	    
-		// Read configuration file
-		try {
-			// Read configuration file
-			myQRMDTO = myReadConfig.getQrManagers(configFilePath);
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			return;
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			return;
-		} catch (ParseException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			return;
-		}
+	    // Read configuration file
+	    try {
+	       // Read QRManager instances from configuration file
+	       myQRMDTO = myReadConfig.getQrManagers(configFilePath);
+	    } catch (FileNotFoundException e1) {
+	       // TODO Auto-generated catch block
+	       e1.printStackTrace();
+	       return;
+	    } catch (IOException e1) {
+	       // TODO Auto-generated catch block
+	       e1.printStackTrace();
+	       return;
+	    } catch (ParseException e1) {
+	       // TODO Auto-generated catch block
+	       e1.printStackTrace();
+	       return;
+	    }
 
-		// Read JSON schema validation elements from configuration
-		try {
-			// Read configuration file
-			schemaElements = myReadConfig.getSchemaValidator(configFilePath);
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			return;
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			return;
-		} catch (ParseException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			return;
-		}		
+	    // Read JSON schema validation elements from configuration
+	    try {
+	       // Read configuration file
+	       schemaElements = myReadConfig.getSchemaValidator(configFilePath);
+	    } catch (FileNotFoundException e1) {
+	       // TODO Auto-generated catch block
+	       e1.printStackTrace();
+	       return;
+	    } catch (IOException e1) {
+	       // TODO Auto-generated catch block
+	       e1.printStackTrace();
+	      return;
+	    } catch (ParseException e1) {
+	       // TODO Auto-generated catch block
+	       e1.printStackTrace();
+	       return;
+	    }		
 		
-		// Get DigitalSerialIOImpl singleton instance to be used 
-		// by all QRManager instances
-		logger.info("-------------------------------\n");
-		dSerialImpl = DigitalSerialIOImpl.getInstance(configFilePath);
+	    // Get DigitalSerialIOImpl singleton instance to be used 
+	    // by all QRManager instances
+	    logger.info("-------------------------------\n");
+	    dSerialImpl = DigitalSerialIOImpl.getInstance(configFilePath);
 
-		for (int i = 0; i < myQRMDTO.size(); i++) {
-			try {
-				// Create QRManager instance based in configuration
-				QRManager qrm = new QRManager(myQRMDTO.get(i), 
-											  dSerialImpl);
-				// Thread.sleep(1000);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			logger.info("-------------------------------\n" +
-								"QRManager instance with parameters " + 
-								"name = " + myQRMDTO.get(i).getName() + 
-								", portName = " + myQRMDTO.get(i).getPortName() + 
-								", speed = " + myQRMDTO.get(i).getSpeed() +  
-								" doChannel = " + myQRMDTO.get(i).getDoChannel() +  
-								" pulseLengthMs = " + 				   myQRMDTO.get(i).getpulseLenghtMs() + " created");
-		}
+	    // Create QRManager instances based in configuration file
+	    for (int i = 0; i < myQRMDTO.size(); i++) {
+	       try {
+	          // Create QRManager instance based in configuration
+		  QRManager qrm = new QRManager(myQRMDTO.get(i), dSerialImpl);
+		  // Thread.sleep(1000);
+	       } catch (Exception e) {
+	          // TODO Auto-generated catch block
+	          e.printStackTrace();
+	       }
+	       logger.info("-------------------------------\n" +
+	                   "QRManager instance with parameters " + 
+		           "name = " + myQRMDTO.get(i).getName() + 
+			   ", portName = " + myQRMDTO.get(i).getPortName() + 
+			   ", speed = " + myQRMDTO.get(i).getSpeed() +  
+			   " doChannel = " + myQRMDTO.get(i).getDoChannel() +  
+			   " pulseLengthMs = " +
+ 			   myQRMDTO.get(i).getpulseLenghtMs() + " created");
+	    }
+            logger.info("-------------------------------\n");
 		
-		// Report QRManager is up and running by blinking pin 13 LED
-		while (true) {
-		   dSerialImpl.setOutputOn(QRMANAGER_NAME, RUN_LED_PIN); 
-		   try {
-		      Thread.sleep(RUN_LED_PERIOD_MS); 
-		   } catch (InterruptedException e) {
-	              // TODO Auto-generated catch block
-		      e.printStackTrace();
-		   } 
-		   dSerialImpl.setOutputOff(QRMANAGER_NAME, RUN_LED_PIN);
-		} 
-	}
-}
+	    // Report QRManager is up and running by blinking pin 13 LED
+	    while (true) {
+	       dSerialImpl.setOutputOn(QRMANAGER_NAME, RUN_LED_PIN); 
+	       try {
+	          Thread.sleep(RUN_LED_PERIOD_MS); 
+	       } catch (InterruptedException e) {
+	          // TODO Auto-generated catch block
+		  e.printStackTrace();
+	       } 
+	       dSerialImpl.setOutputOff(QRMANAGER_NAME, RUN_LED_PIN);
+	    } 
+	} // End of main() method
+} // End of QRManager class
