9ecffe8bc1b367d758afbd9fff778045b9e7307a
Roberto Fernandez <fernandezbrm@gmail.com>
2024-02-04 20:06:32 -0600

    WiegandTest: Clean up door lock ACTIVE/INACTIVE state logic

--- arduino/WiegandTest/WiegandTest.ino
+++ arduino/WiegandTest/WiegandTest.ino
@@ -1,19 +1,23 @@
 #include "Wiegand.h"
 
+// Door lock configuration
 const int DOOR_LOCK = 10;
 const int DOOR_LOCK_DELAY = 2000;
+// Door lock shall always be active. NC relay output to be used.
+const int ACTIVE   = 1;
+const int INACTIVE = 0;
 
-const int ACTIVE   = 0;
-const int INACTIVE = 1;
-
+// Wiegand input configuration
 const int W1_D0 = 2;
 const int W1_D1 = 3;
 const int W2_D0 = 4;
 const int W2_D1 = 5;
 
+// Wiegand reader instances
 WIEGAND wg1;
 WIEGAND wg2;
 
+// Valid cards to be granted access
 unsigned long validCards[] = {
   0x472B03E5,
   1234
@@ -23,7 +27,7 @@ void setup() {
 	Serial.begin(9600);  
   pinMode(DOOR_LOCK, OUTPUT);
   // Door lock starts on
-  digitalWrite(DOOR_LOCK, ACTIVE);
+  digitalWrite(DOOR_LOCK, INACTIVE);
    
 	// default Wiegand Pin 2 and Pin 3 see image on README.md
 	// for non UNO board, use wg.begin(pinD0, pinD1) where pinD0 and pinD1 
@@ -35,9 +39,9 @@ void setup() {
 }
 
 void openDoorLock() {
-  digitalWrite(DOOR_LOCK, INACTIVE);
-  delay(DOOR_LOCK_DELAY);
   digitalWrite(DOOR_LOCK, ACTIVE);
+  delay(DOOR_LOCK_DELAY);
+  digitalWrite(DOOR_LOCK, INACTIVE);
 }
 
 void loop() {
@@ -54,16 +58,14 @@ void loop() {
     numCards = sizeof(validCards)/sizeof(unsigned long);
     validCard = false;
     for (int i = 0; i < numCards; i++) {
-       // Serial.println("validCards = ");
-       // Serial.println(validCards[i], HEX);
-       if (wg1.getCode() == validCards[i]) {
+      if (wg1.getCode() == validCards[i]) {
         Serial.print("READER 1: Valid card read, ");
         Serial.print(validCards[i], HEX);
         Serial.println(", release door lock");
         validCard = true;
         openDoorLock();
         break;
-       }      
+      }      
     }
     if (!validCard) {
       Serial.print("READER 1: INVALID card read, ");
